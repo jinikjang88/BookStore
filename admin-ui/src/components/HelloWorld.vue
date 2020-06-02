1a3a16e169d283bb9ff0fc5465250797ca7300d1
mz_jangjinik <mz_jangjinik@woowafriends.com>
2020-06-02 23:58:37 +0900

    다음지도 넣기 테스트

---
+++ admin-ui/src/components/HelloWorld.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    {{ text }}
+    <div id="map" style="width:500px;height:400px;"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: () => ({
+    text: "사랑해요 박빛나"
+  }),
+  mounted() {
+    if (window.kakao && window.kakao.maps) {
+      this.initMap();
+    } else {
+      const script = document.createElement('script');
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = 'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=2703d008acfd312ffabb10166bc37173';
+      document.head.appendChild(script);
+    }
+  },
+  methods: {
+    initMap() {
+      var container = document.getElementById('map');
+      var options = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+      };
+
+      var map = new kakao.maps.Map(container, options);
+      map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
